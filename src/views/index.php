3442e27eb007fd5fed99978f9e30296f6f7a551e
João Félix <felixjoao651@gmail.com>
2023-07-26 14:22:58 -0400

    Aplicando Padrão MVC

---
+++ src/views/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once(__DIR__ . "/../../templates/header.php");
+require_once(__DIR__ . "/../../config/connection.php");
+?>
+
+<h1>OLá Mundo</h1>
+
+<?php
+require_once(__DIR__ . "/../../templates/footer.php");
+?>

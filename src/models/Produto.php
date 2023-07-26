7f83605e796732e69c020365e9c725535a20083c
João Félix <felixjoao651@gmail.com>
2023-07-26 19:23:27 -0400

    Cria a classe Produto com getters e setters

--- src/models/Produto.php
+++ src/models/Produto.php
@@ -1,1 +1,73 @@
 <?php
+
+namespace Felix\ECommerce\Models;
+
+class Produto
+{
+
+  private $id;
+  private $nome;
+  private $descricao;
+  private $preco;
+  private $imagem;
+
+  public function __construct($nome = '', $descricao = '', $preco = '', $imagem = '')
+  {
+    $this->nome = $nome;
+    $this->descricao = $descricao;
+    $this->preco = $preco;
+    $this->imagem = $imagem;
+  }
+
+  // Métodos Getters e Setters (acessores e modificadores) para cada propriedade
+
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function getNome()
+  {
+    return $this->nome;
+  }
+
+  public function getDescricao()
+  {
+    return $this->descricao;
+  }
+
+  public function getPreco()
+  {
+    return $this->preco;
+  }
+
+  public function getImagem()
+  {
+    return $this->imagem;
+  }
+
+  public function setId($id)
+  {
+    return $this->id = $id;
+  }
+
+  public function setNome($nome)
+  {
+    return $this->nome = $nome;
+  }
+
+  public function setDescricao($descricao)
+  {
+    return $this->descricao = $descricao;
+  }
+
+  public function setPreco($preco)
+  {
+    return $this->preco = $preco;
+  }
+
+  public function setImagem($imagem)
+  {
+    return $this->imagem = $imagem;
+  }
+}

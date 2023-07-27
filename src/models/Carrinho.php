f541d0592d46f311ba0ef749701fcb7c051a83d3
João Félix <felixjoao651@gmail.com>
2023-07-26 23:44:16 -0400

    adicionando metodos para o carrinho de compras

--- src/models/Carrinho.php
+++ src/models/Carrinho.php
@@ -1,1 +1,43 @@
 <?php
+
+namespace Felix\ECommerce\Models;
+
+class Carrinho
+{
+  private $itens;
+
+  public function __construct()
+  {
+    $this->itens = array();
+  }
+
+  public function adicionarProduto($produto)
+  {
+    $this->itens[] = $produto;
+  }
+
+  public function removerProduto($produtoId)
+  {
+    foreach ($this->itens as $key => $item) {
+      if ($item->getId() == $produtoId) {
+        unset($this->itens[$key]);
+        break;
+      }
+    }
+  }
+
+  public function calcularTotal()
+  {
+    $total = 0;
+    foreach ($this->itens as $item) {
+      $total += $item->getPreco();
+    }
+
+    return $total;
+  }
+
+  public function getItens()
+  {
+    return $this->itens;
+  }
+}

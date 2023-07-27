192e9a714be065d83616cca53aac4354e0702475
João Félix <felixjoao651@gmail.com>
2023-07-27 12:42:22 -0400

    Iniciando Back-end na pagina do carrinho

--- src/views/carrinho.php
+++ src/views/carrinho.php
@@ -1,46 +1,66 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php';
 require_once(__DIR__ . "/../../templates/header.php");
+require_once("../config/url.php");
+
+use Felix\ECommerce\Config\Connection;
+use Felix\ECommerce\Controllers\CarrinhoController;
+use Felix\ECommerce\Models\Produtos;
+
+$database = new Connection();
+$conn = $database->getConnection();
+
+$produtosModel = new Produtos($conn);
+$carrinhoController = new CarrinhoController($produtosModel);
+
+$carrinhoController->add(2);
+$itensCarrinho = $carrinhoController->index();
+
 ?>
 
-<main class="bg-primary">
-  <div class="container">
-    <h1>Seu Carrinho</h1>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>Imagem</th>
-          <th>Nome do Produto</th>
-          <th>Preço Unitário</th>
-          <th>Quantidade</th>
-          <th>Subtotal</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- Vamos assumir que temos um produto no carrinho -->
-        <tr>
-          <td><img src="<?= $BASE_URL ?>assets/img/foto1.jpg" alt="Imagem do Produto" width="100"></td>
-          <td>Nome do Produto</td>
-          <td>R$ 99,99</td>
-          <td>1</td>
-          <td>R$ 99,99</td>
-          <td>
-            <a href="#" class="btn btn-primary">Editar</a>
-            <a href="#" class="btn btn-danger">Remover</a>
-          </td>
-        </tr>
-        <!-- Mais produtos iriam aqui -->
-      </tbody>
-    </table>
-    <div class="text-right">
-      <h4>Total: R$ 99,99</h4>
+<?php if (!empty($itensCarrinho)) : ?>
+  <main class="bg-primary">
+    <div class="container">
+      <h1>Seu Carrinho</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Imagem</th>
+            <th>Nome do Produto</th>
+            <th>Preço Unitário</th>
+            <th>Quantidade</th>
+            <th>Subtotal</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($itensCarrinho as $item) : ?>
+            <tr>
+              <td><img src="<?= $BASE_URL . $item->getImagem() ?>" alt="Imagem do Produto" width="100"></td>
+              <td><?= $item->getNome() ?></td>
+              <td>R$ <?= $item->getPreco() ?></td>
+              <td>1</td> <!-- Aqui você precisará implementar a lógica para quantidades -->
+              <td>R$ <?= $item->getPreco() ?></td> <!-- E aqui o subtotal para cada produto -->
+              <td>
+                <a href="#" class="btn btn-primary">Editar</a>
+                <a href="#" class="btn btn-danger">Remover</a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      <?php else : ?>
+        <p>Nenhum item no carrinho.</p>
+      <?php endif; ?>
+      </table>
+      <div class="text-right">
+        <h4>Total: R$ <?= $carrinhoController->getCarrinho()->calcularTotal() ?></h4>
+      </div>
+      <div class="text-right mt-3">
+        <a href="#" class="btn btn-success">Finalizar Compra</a>
+      </div>
     </div>
-    <div class="text-right mt-3">
-      <a href="#" class="btn btn-success">Finalizar Compra</a>
-    </div>
-  </div>
-</main>
+  </main>
 
-<?php
-require_once(__DIR__ . "/../../templates/footer.php");
-?>
+  <?php
+  require_once(__DIR__ . "/../../templates/footer.php");
+  ?>

202040434b970ea2e168263de19b8210276a98d8
João Félix <felixjoao651@gmail.com>
2023-07-27 01:13:29 -0400

    iniciando aplicação back-end no index

--- src/views/index.php
+++ src/views/index.php
@@ -2,112 +2,47 @@
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once(__DIR__ . "/../../templates/header.php");
+require_once("../config/url.php");
 
 use Felix\ECommerce\Config\Connection;
+use Felix\ECommerce\Controllers\ProdutoController;
+use Felix\ECommerce\Models\Produtos;
 
 //instância a classe connection e obtenha a conexão
 $database = new Connection();
 $conn = $database->getConnection();
 
+$produtosModel = new Produtos($conn);
+$produtoController = new ProdutoController($produtosModel);
+
+$produtos = $produtoController->index();
 ?>
 
 <main class="bg-primary">
   <div class="container pt-5 pb-5">
     <div class="row">
 
-      <!-- Produto 1 -->
-      <div class="col-md-4">
-        <div class="card p-3">
-          <img src="<?= $BASE_URL ?>assets/img/foto1.jpg" class="card-img-top" alt="Imagem do Produto 1">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Mini-Teclado para jogos</h3>
-            <br>
-            <h4 class="card-text">R$ 229,99</h4>
-            <p class="card-text">Redragon K617 Fizz 60% RGB com fio, teclado mecânico compacto de 61 teclas.</p>
-            <a href="#" class="btn btn-primary mt-auto">Adicionar ao carrinho</a>
-          </div>
-        </div>
-      </div>
+      <?php foreach ($produtos as $produto) : ?>
 
-      <!-- Produto 2 -->
-      <div class="col-md-4">
-        <div class="card p-3">
-          <img src="<?= $BASE_URL ?>assets/img/foto1.jpg" class="card-img-top" alt="Imagem do Produto 2">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Mini-Teclado para jogos</h3>
-            <h4 class="card-text">R$ 229,99</h4>
-            <br>
-            <p class="card-text">Redragon K617 Fizz 60% RGB com fio, teclado mecânico compacto de 61 teclas.</p>
-            <a href="#" class="btn btn-primary mt-auto">Adicionar ao carrinho</a>
+        <!-- Produto -->
+        <div class="col-md-4">
+          <div class="card p-3">
+            <img src="<?= $BASE_URL . $produto->getImagem() ?>" class="card-img-top" alt="Imagem do Produto <?= $produto->getId() ?>">
+            <?php var_dump($BASE_URL . $produto->getImagem()); ?>
+            <div class="card-body d-flex flex-column">
+              <h3 class="card-title"><?= $produto->getNome() ?></h3>
+              <h4 class="card-text">R$ <?= $produto->getPreco() ?></h4>
+              <p class="card-text"><?= $produto->getDescricao() ?></p>
+              <a href="#" class="btn btn-primary mt-auto">Adicionar ao carrinho</a>
+            </div>
           </div>
         </div>
-      </div>
+      <?php endforeach; ?>
 
-      <!-- Produto 3 -->
-      <div class="col-md-4">
-        <div class="card p-3">
-          <img src="<?= $BASE_URL ?>assets/img/foto1.jpg" class="card-img-top" alt="Imagem do Produto 3">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Mini-Teclado para jogos</h3>
-            <h4 class="card-text">R$ 229,99</h4>
-            <br>
-            <p class="card-text">Redragon K617 Fizz 60% RGB com fio, teclado mecânico compacto de 61 teclas.</p>
-            <a href="#" class="btn btn-primary mt-auto">Adicionar ao carrinho</a>
-          </div>
-        </div>
-      </div>
 
     </div>
-
-    <div class="row mt-4">
-
-      <!-- Produto 4 -->
-      <div class="col-md-4">
-        <div class="card p-3">
-          <img src="<?= $BASE_URL ?>assets/img/foto1.jpg" class="card-img-top" alt="Imagem do Produto 4">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Mini-Teclado para jogos</h3>
-            <h4 class="card-text">R$ 229,99</h4>
-            <br>
-            <p class="card-text">Redragon K617 Fizz 60% RGB com fio, teclado mecânico compacto de 61 teclas.</p>
-            <a href="#" class="btn btn-primary mt-auto">Adicionar ao carrinho</a>
-          </div>
-        </div>
-      </div>
-
-      <!-- Produto 5 -->
-      <div class="col-md-4">
-        <div class="card p-3">
-          <img src="<?= $BASE_URL ?>assets/img/foto1.jpg" class="card-img-top" alt="Imagem do Produto 5">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Mini-Teclado para jogos</h3>
-            <h4 class="card-text">R$ 229,99</h4>
-            <br>
-            <p class="card-text">Redragon K617 Fizz 60% RGB com fio, teclado mecânico compacto de 61 teclas.</p>
-            <a href="#" class="btn btn-primary mt-auto">Adicionar ao carrinho</a>
-          </div>
-        </div>
-      </div>
-
-      <!-- Produto 6 -->
-      <div class="col-md-4">
-        <div class="card p-3">
-          <img src="<?= $BASE_URL ?>assets/img/foto1.jpg" class="card-img-top" alt="Imagem do Produto 6">
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">Mini-Teclado para jogos</h3>
-            <h4 class="card-text">R$ 229,99</h4>
-            <br>
-            <p class="card-text">Redragon K617 Fizz 60% RGB com fio, teclado mecânico compacto de 61 teclas.</p>
-            <a href="#" class="btn btn-primary mt-auto">Adicionar ao carrinho</a>
-          </div>
-        </div>
-      </div>
-    </div>
-
   </div>
-
 </main>
-
 
 <?php
 require_once(__DIR__ . "/../../templates/footer.php");

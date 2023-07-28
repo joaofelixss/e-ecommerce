1491d3478b2790ad5e48345ddf1a3da0873d7f45
João Félix <felixjoao651@gmail.com>
2023-07-27 21:52:41 -0400

    Adicionando Front-end inicial na página produto para iniciar a versão 2.0

--- src/views/produto.php
+++ src/views/produto.php
@@ -1,44 +1,83 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once(__DIR__ . "/../../templates/header.php");
+require_once("../config/url.php");
 
 use Felix\ECommerce\Config\Connection;
 use Felix\ECommerce\Controllers\ProdutoController;
 use Felix\ECommerce\Models\Produtos;
 
-$id = $_GET['id'];
-
+// Obtendo a conexão do banco de dados
 $database = new Connection();
 $conn = $database->getConnection();
 
 $produtosModel = new Produtos($conn);
 $produtoController = new ProdutoController($produtosModel);
 
-$produto = $produtoController->show($id);
+$idProduto = $_GET['id']; 
+$produto = $produtoController->show($idProduto);
+?>
 
-if ($produto !== null) : ?>
-  <!-- Detalhes do Produto -->
-  <main>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <img src="<?= $BASE_URL . $produto->getImagem() ?>" class="img-fluid" alt="Imagem do Produto">
-        </div>
-        <div class="col-md-6 mt-3">
-          <h2><?= $produto->getNome() ?></h2>
-          <p class="mt-2"><?= $produto->getDescricao() ?></p>
-          <p class="mt-2">R$ <?= $produto->getPreco() ?></p>
-          <form method="post" action="carrinho.php" class="mt-2">
-            <input type="hidden" name="add" value="<?= $produto->getId() ?>">
-            <input type="submit" class="btn btn-primary" value="Adicionar ao Carrinho">
-          </form>
-        </div>
+<main>
+  <div class="container mt-5">
+    <div class="row">
+      <!-- Detalhes do Produto -->
+      <div class="col-md-6">
+        <!-- Imagens do Produto -->
+        <img src="<?= $BASE_URL . $produto->getImagem() ?>" class="img-fluid" alt="Imagem do Produto">
+
+        <!-- Nome, Descrição e Preço -->
+        <h2><?= $produto->getNome() ?></h2>
+        <p><?= $produto->getDescricao() ?></p>
+        <p>R$ <?= $produto->getPreco() ?></p>
+
+        <!-- Botão para adicionar ao carrinho -->
+        <form method="post" action="../views/carrinho.php">
+          <input type="hidden" name="add" value="<?= $produto->getId() ?>">
+          <input type="submit" value="Adicionar ao Carrinho">
+        </form>
       </div>
-    </div>
-  </main>
-<?php else : ?>
-  <p>Produto não encontrado.</p>
-<?php endif;
 
+      <!-- Descrição detalhada -->
+      <div class="col-md-12 mt-5">
+        <h4>Descrição detalhada</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+      </div>
+
+      <!-- Imagens adicionais -->
+      <div class="col-md-12 mt-5">
+        <h4>Imagens adicionais</h4>
+        <!-- Adicionar suas imagens adicionais aqui -->
+      </div>
+
+      <!-- Avaliações de clientes -->
+      <div class="col-md-12 mt-5">
+        <h4>Avaliações de clientes</h4>
+        <!-- Adicionar avaliações de clientes aqui -->
+      </div>
+
+      <!-- Detalhes de envio e retorno -->
+      <div class="col-md-12 mt-5">
+        <h4>Detalhes de envio e retorno</h4>
+        <p>Envio dentro de 2-3 dias. Retorno aceito dentro de 14 dias.</p>
+      </div>
+
+      <!-- FAQ -->
+      <div class="col-md-12 mt-5">
+        <h4>Perguntas Frequentes</h4>
+        <p>Q: Isso é um produto de exemplo?<br>A: Sim, isso é apenas um produto de exemplo.</p>
+      </div>
+
+      <!-- Produtos relacionados -->
+      <div class="col-md-12 mt-5">
+        <h4>Produtos relacionados</h4>
+        <!-- Adicionar produtos relacionados aqui -->
+      </div>
+
+    </div>
+  </div>
+</main>
+
+<?php
 require_once(__DIR__ . "/../../templates/footer.php");
 ?>

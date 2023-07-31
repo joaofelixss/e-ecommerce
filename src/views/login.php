b386d5b70ff6af620bd533867beff5c52309e95a
João Félix <felixjoao651@gmail.com>
2023-07-31 17:35:42 -0400

    Modificando interface de login

--- src/views/login.php
+++ src/views/login.php
@@ -6,18 +6,22 @@ require_once('../../templates/header.php');
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-md-6">
-      <h2 class="text-center mt-5">Login de Admin</h2>
-      <form action="../controllers/AdminController.php" method="post" class="p-4 mt-3 border rounded">
-        <div class="form-group">
-          <label for="username">Nome de usuário:</label>
-          <input type="text" id="username" name="username" required class="form-control">
+      <div class="card mt-5">
+        <h2 class="card-header text-center text-dark">Login de Admin</h2>
+        <div class="card-body">
+          <form action="../controllers/AdminController.php" method="post">
+            <div class="form-group mb-3">
+              <label for="username" class="text-dark">Nome de usuário:</label>
+              <input type="text" id="username" name="username" required class="form-control">
+            </div>
+            <div class="form-group mb-3">
+              <label for="password" class="text-dark">Senha:</label>
+              <input type="password" id="password" name="password" required class="form-control">
+            </div>
+            <input type="submit" value="Entrar" class="btn btn-dark btn-block">
+          </form>
         </div>
-        <div class="form-group">
-          <label for="password">Senha:</label>
-          <input type="password" id="password" name="password" required class="form-control">
-        </div>
-        <input type="submit" value="Entrar" class="btn btn-primary btn-block">
-      </form>
+      </div>
     </div>
   </div>
 </div>

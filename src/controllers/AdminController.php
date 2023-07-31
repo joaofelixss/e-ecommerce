b9ef68ae9362054879dd71f861e6233e64aef392
João Félix <felixjoao651@gmail.com>
2023-07-31 17:30:12 -0400

    Adicionando verificações e validações de usuário admin

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace Felix\ECommerce\Controllers;
 
+use Felix\ECommerce\Config\Connection;
 use Felix\ECommerce\Models\Admin;
 
 class AdminController
@@ -20,6 +21,7 @@ class AdminController
   {
     if ($this->adminModel->findAdmin($username, $password)) {
       $_SESSION['admin'] = $username;
+      $_SESSION['success'] = "Login efetuado com sucesso";
       header('Location: admin.php');
     } else {
       echo "Usuário ou senha incorretos.";
@@ -31,4 +33,21 @@ class AdminController
     unset($_SESSION['admin']);
     header('Location: login.php');
   }
+}
+
+// Processamento do Formulário de Login
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  include '../models/Admin.php';
+  include '../config/connection.php';
+
+  // Instancie a classe Connection e obtenha a conexão
+  $connection = (new Connection())->getConnection();
+
+  $adminModel = new \Felix\ECommerce\Models\Admin($connection);
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $adminController = new AdminController($adminModel);
+  $adminController->login($username, $password);
 }
--- src/models/Admin.php
+++ src/models/Admin.php
@@ -11,17 +11,44 @@ class Admin
     $this->conn = $connection;
   }
 
+  public function insertAdmin($username, $password)
+  {
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO admins (username, password) VALUES (:username, :password)";
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":password", $passwordHash);
+
+    try {
+      $stmt->execute();
+      return true;
+    } catch (\PDOException $e) {
+      echo $e->getMessage();
+      return false;
+    }
+  }
+
   public function findAdmin($username, $password)
   {
-    $stmt = $this->conn->prepare("SELECT * FROM admins WHERE username = :username AND password = :password");
-    $stmt->bindParam(":username", $username);
-    $stmt->bindParam(":password", $password);
-    $stmt->execute();
+    $query = "SELECT * FROM admins WHERE username = :username";
+    $stmt = $this->conn->prepare($query);
 
-    $row = $stmt->fetch(\PDO::FETCH_ASSOC);
-    if ($row) {
-      return true;
+    $stmt->bindParam(":username", $username);
+
+    try {
+      $stmt->execute();
+      $user = $stmt->fetch();
+
+      if ($user && password_verify($password, $user['password'])) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (\PDOException $e) {
+      echo $e->getMessage();
+      return false;
     }
-    return false;
   }
 }

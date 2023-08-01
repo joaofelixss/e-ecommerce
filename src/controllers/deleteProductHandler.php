37a04d7d8a65114a4fcd29c85d50c32f73a40887
João Félix <felixjoao651@gmail.com>
2023-07-31 20:43:31 -0400

    Adicionando arquivo para deletar produtos

---
+++ src/controllers/deleteProductHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+use Felix\ECommerce\Config\Connection;
+use Felix\ECommerce\Models\Produtos;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $database = new Connection();
+  $conn = $database->getConnection();
+
+  $produtosModel = new Produtos($conn);
+
+  $id = $_POST['id'];
+
+  $produtosModel->excluirProduto($id);
+
+  header('Location: ../views/index.php');
+  exit;
+}
